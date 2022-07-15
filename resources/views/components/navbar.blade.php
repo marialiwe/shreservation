98983c73cd2e73ecd180a1049b248bdee27840b9
maria <maria.422020025@civitas.ukrida.ac.id>
2022-07-15 19:44:13 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{route('welcome')}}">
+                <img src="{{asset('images/logo4.png')}}" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#facilities">Facilities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#reservation">Reservation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testi">Testimony</a>
+                    </li>
+                </ul>
+                @auth
+                <div class="d-flex user-logged nax-item dropdown no-arrow">
+                    <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        Halo, {{ Auth::user()->name }}
+                        @if (Auth::user()->avatar)
+                        <img src="{{Auth::user()->avatar}}" class="user-photo" alt="" style="border-radius: 50%">
+                        @else
+                        <img src="https://ui-avatars.com/api/?name=Admin" class="user-photo" alt="" style="border-radius: 50%">
+                        @endif
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto;">
+                            <li>
+                                <a href="{{ route('dashboard') }}" class="dropdown-item">My Dashboard</a>
+                            </li>
+                            <li>
+                            @if (Auth::user()->is_admin)
+                                <li>
+                                    <a href="{{route('admin.discount.index')}}" class="dropdown-item">Discount</a>
+                                </li>
+                            @endif                                
+                                <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign Out</a>
+                            </li>    
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            </form>
+                        </ul>
+                    </a>
+                </div>
+                @else
+                <div class="d-flex">
+                    <a href="{{route('login')}}" class="btn btn-master btn-secondary me-3">
+                        Sign In
+                    </a>
+                    <a href="/login" class="btn btn-master btn-primary">
+                        Sign Up
+                    </a>
+                </div>
+                @endauth
+            </div>
+        </div>
+    </nav>
